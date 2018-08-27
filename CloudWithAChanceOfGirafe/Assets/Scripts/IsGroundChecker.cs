b35d4d18f858421780abd21555946a6a3a2644d5
Taichi <m011534042@edu.teu.ac.jp>
2018-08-27 15:40:33 +0900

    InportSprites. Set provisional background Create scripts PlayerMove,IsGroundChecker

---
+++ CloudWithAChanceOfGirafe/Assets/Scripts/IsGroundChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsGroundChecker : MonoBehaviour {
+
+    public bool _isGroundCheck;
+
+	void Update () {
+		
+	}
+    private void OnTriggerStay(Collider collision)
+    {
+        Debug.Log("OnGround!");
+        if (collision.tag != "Player")
+        {
+        
+
+        }
+        
+    }
+
+}
---
+++ CloudWithAChanceOfGirafe/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    Rigidbody2D myrigid;
+
+    [SerializeField]
+    [Header("junp power")]
+    private float junpPower=1.0f;
+
+
+    [SerializeField]
+    [Header("side power")]
+    private float sidePower = 1.0f;
+
+    //----Infinite jump prohibition----//
+    [SerializeField]
+    [Header("Foot colloder")]
+    private bool isGround;
+
+    private IsGroundChecker _IsGroundChecker;
+
+    void Start () {
+        myrigid = GetComponent<Rigidbody2D>();
+        _IsGroundChecker = GetComponentInChildren<IsGroundChecker>();
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Move();
+	}
+
+    void Move()
+    {
+        if (Input.GetAxis("Horizontal")!=0)
+        {
+            myrigid.AddForce(new Vector2(Input.GetAxis("Horizontal")* sidePower,0));
+        }
+
+
+        if (Input.GetKey(KeyCode.Space)&&_IsGroundChecker._isGroundCheck == true)
+        {
+            myrigid.AddForce(Vector2.up * junpPower);
+        }
+
+    }
+}
