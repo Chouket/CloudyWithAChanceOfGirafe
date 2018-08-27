de25453d99555cf223d9268525b0a019dc9a99aa
Thomas <t.aubart@student.isartdigital.com>
2018-08-27 08:37:29 +0200

    MainMenu Scene

---
+++ CloudWithAChanceOfGirafe/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager instance = null;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (!instance)
+                instance = new GameManager();
+
+            return instance;
+        }
+    }
+    #endregion
+
+    private void Awake()
+    {
+        if (instance && instance != this)
+            Destroy(gameObject);
+    }
+
+
+
+    [SerializeField] string m_gameplaySceneName = "";
+
+    public void Play()
+    {
+        SceneManager.LoadScene(m_gameplaySceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ CloudWithAChanceOfGirafe/Assets/Scripts/Manager/MainMenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    GameManager m_gameManager;
+
+    private void Start()
+    {
+        m_gameManager = GameManager.Instance;
+    }
+
+    public void PlayButtonPressed()
+    {
+        m_gameManager.Play();
+    }
+
+    public void QuitButtonPressed()
+    {
+        m_gameManager.Quit();
+    }
+}
