e1acdf712f2c0b31e3c9687c077af797ededf460
Thomas <t.aubart@student.isartdigital.com>
2018-08-27 12:26:16 +0200

    merge with playerMovement

---
+++ CloudWithAChanceOfGirafe/Assets/Scripts/IsGroundChecker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsGroundChecker : MonoBehaviour {
+
+    public bool _isGroundCheck;
+
+	void Update () {
+		
+	}
+    private void OnTriggerStay(Collider collision)
+    {
+        Debug.Log("OnGround!");
+
+        if (collision.tag != "Player")
+        {
+            _isGroundCheck = true;
+
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        _isGroundCheck = false;
+    }
+
+}
---
+++ CloudWithAChanceOfGirafe/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    Rigidbody2D myrigid;
+
+    [SerializeField]
+    [Header("junp power")]
+    private float junpPower=1.0f;
+
+
+    [SerializeField]
+    [Header("side power")]
+    private float sidePower = 1.0f;
+
+    //----Infinite jump prohibition----//
+    [SerializeField]
+    [Header("Foot colloder")]
+    private bool isGround;
+
+    //-----use Animation-----//
+    public Animator anim;
+
+
+    //-----Ground Check--//
+    private IsGroundChecker _IsGroundChecker;
+
+    //----Player Animation Enum---//
+    public enum PlayerAnimationState
+    {
+        IDLE,
+        JUMP,
+        JUMPING,
+
+    }
+    public PlayerAnimationState _PlayerAnimationState;
+
+    void Start () {
+        myrigid = GetComponent<Rigidbody2D>();
+        _IsGroundChecker = GetComponentInChildren<IsGroundChecker>();
+
+    }
+	
+	//----use rigidBody Update--//
+	void FixedUpdate () {
+        Move();
+        
+	}
+
+
+    private void Update()
+    {
+        Animation();
+    }
+
+    void Move()
+    {
+        
+        if (Input.GetAxis("Horizontal")!=0)
+        {
+            myrigid.AddForce(new Vector2(Input.GetAxis("Horizontal")* sidePower,0));
+        }
+
+        //--------
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //-----jumping Animation----//
+            _PlayerAnimationState = PlayerAnimationState.JUMP;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (_IsGroundChecker._isGroundCheck == true)
+            {
+                //------jumping-----//
+                myrigid.AddForce(Vector2.up * junpPower);
+            }
+
+            //-----jumping Animation----//
+            _PlayerAnimationState = PlayerAnimationState.JUMPING;
+        }
+
+    }
+
+    void Animation()
+    {
+        switch (_PlayerAnimationState)
+        {
+            case PlayerAnimationState.IDLE:
+                anim.Play("PlayerAnimstion");
+                break;
+            case PlayerAnimationState.JUMP:
+                anim.Play("PlayerJump");
+                break;
+            case PlayerAnimationState.JUMPING:
+                anim.Play("PlayerJumping");
+
+                if(_IsGroundChecker._isGroundCheck == true) _PlayerAnimationState = PlayerAnimationState.IDLE;
+
+                break;
+            default:
+                break;
+        }
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            
+        }
+        else
+        {
+           
+        }
+    }
+}
