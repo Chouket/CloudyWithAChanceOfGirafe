2a52a8dd84ddabb52889cc20eae710bfd0266e54
Unknown <artydapotato@gmail.com>
2018-08-27 06:17:29 -0400

    Spawner Update

---
+++ CloudWithAChanceOfGirafe/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+	[SerializeField]
+	List<List<GameObject>> m_objects;
+
+	[SerializeField]
+	GameObject m_spawner;
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
--- CloudWithAChanceOfGirafe/Assets/Scripts/Spawner.cs
+++ CloudWithAChanceOfGirafe/Assets/Scripts/Spawner.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-	[SerializeField]
+    [SerializeField]
+    int m_spawnerOffsetY = 0;
 
-	void Start ()
+	[SerializeField]
+	List<GameObject> m_objects;
+
+	[SerializeField]
+	float m_minSize = 0.5f;
+
+	[SerializeField]
+	float m_maxSize = 1.5f;
+
+
+	Camera m_camera;
+
+    void Awake()
 	{
-		
-	}
-	
-	void Update ()
+		Vector3 pos = transform.localPosition;
+        pos.y = m_spawnerOffsetY;
+        transform.localPosition = pos;
+    }
+
+	void Start()
 	{
-		
+		m_camera = Camera.main;
+		Spawn();
 	}
+
+	void Spawn()
+    {
+		float size = Random.Range(m_minSize,m_maxSize);
+		Debug.Log(size);
+		int rand = Random.Range(0, m_objects.Count);
+		int rotate = Random.Range(0, 360);
+
+		Vector3 scale = m_objects[rand].transform.localScale;
+		scale *= size;
+
+		int cameraSize = (int )(m_camera.orthographicSize - scale.x/2);
+		float posX = Random.value * (cameraSize * 2) - cameraSize;
+
+		Vector3 pos = transform.position;
+		pos.x = posX;
+		pos.z = 0f;
+
+		
+		GameObject gao = Instantiate(m_objects[rand], pos, Quaternion.Euler(0f,0f,rotate));
+		
+		gao.transform.localScale = scale;
+    }
+
+	private void Update()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Spawn();
+		}
+	}
+
+	void SetList(List<GameObject> list)
+    {
+		m_objects = list;
+    }
 }
