28e183bb664f5dedac452044404663b1bcb33ea2
Taichi <m011534042@edu.teu.ac.jp>
2018-08-28 01:47:01 +0900

    Interrupt and merge

--- CloudWithAChanceOfGirafe/Assets/Scripts/BackGroundInstance.cs
+++ CloudWithAChanceOfGirafe/Assets/Scripts/BackGroundInstance.cs
@@ -5,13 +5,17 @@ using UnityEngine.UI;
 
 public class BackGroundInstance : MonoBehaviour {
 
+    [Header("0 = Stage1," +
+            "1 = Stage1 into Stage2,")]
     public GameObject[] _backGrounds;
 
+    public Transform[] CheckPoint;
+
     [Header("Imageの高さ")]
-    public float ImageHeight;
+    public float ImageHeight = 15.8f;
 
     [Header("インスタンスする間隔")]
-    public float reroadReach;
+    public float reroadReach = 10;
 
     public Transform cameraPos;
 
@@ -28,6 +32,8 @@ public class BackGroundInstance : MonoBehaviour {
 
     void Instance()
     {
+
+
        if((cameraPos.position.y%ImageHeight)>=reroadReach&& InstanceBackgroundImage.Count < 3)
         {
             InstanceBackgroundImage.Add(_backGrounds[0]);
