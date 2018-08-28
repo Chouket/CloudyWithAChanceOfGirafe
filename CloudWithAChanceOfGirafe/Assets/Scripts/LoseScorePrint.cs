0bfe2edab0ce7d4ed9d6103146e3af98cc55b084
Thomas <t.aubart@student.isartdigital.com>
2018-08-28 07:09:42 +0200

    Score working !

---
+++ CloudWithAChanceOfGirafe/Assets/Scripts/LoseScorePrint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoseScorePrint : MonoBehaviour
+{
+    public Text currScore;
+    public Text currHighScore;
+
+    // Use this for initialization
+    void Awake()
+    {
+        ScoreManager scoreMgr = GameObject.FindObjectOfType<ScoreManager>();
+
+        currScore.text = scoreMgr.scoreValue.ToString();
+        currHighScore.text = scoreMgr.GetHighScore().ToString();
+    }
+}
